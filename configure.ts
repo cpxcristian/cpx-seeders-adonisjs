17b28fe10f432199a7fcae39a30900eb87729030
Cristian Mendoza <cristian.mendoza@copixil.com>
2026-04-20 12:15:26 -0600

    (fix) Al configurar los comandos apunta a l nombre del package

--- configure.ts
+++ configure.ts
@@ -3,6 +3,6 @@ import type Configure from '@adonisjs/core/commands/configure'
 export async function configure(command: Configure) {
   const codemods = await command.createCodemods()
   await codemods.updateRcFile((transformer) => {
-    transformer.addCommand('@cpxproject/seeders/commands')
+    transformer.addCommand('@cpxproject/seeders-adonisjs/commands')
   })
 }
